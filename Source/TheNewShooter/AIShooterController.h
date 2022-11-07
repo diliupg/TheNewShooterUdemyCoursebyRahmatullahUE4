0619aa16438fd96787732dd9863cf9dcc495ff2c
diliupg <unitygamecodersl@gmail.com>
2022-11-07 14:27:52 +0530

    added the AIShooterController.cpp file

---
+++ Source/TheNewShooter/AIShooterController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIShooterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THENEWSHOOTER_API AAIShooterController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
